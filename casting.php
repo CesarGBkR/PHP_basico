857236a6cf9000ea68df8ef96342ea776b847f5b
CesarGBkR <yexotortuesprin@gmail.com>
2022-07-26 11:07:52 -0500

    Casting

---
+++ casting.php
@@ -0,0 +1,20 @@
+<?php
+$numero="5";
+var_dump($numero);
+
+$numero=(int)$numero;
+var_dump($numero);
+
+$dias=5.89;
+var_dump($dias);
+
+$dias=(int)$dias;
+var_dump($dias);
+
+$bandera=0;
+var_dump($bandera);
+
+$bandera=(bool)$bandera;
+var_dump($bandera);
+
+echo "\n";
