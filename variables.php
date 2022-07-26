b87f08dc30cbfc3c1aacb3e82a6a1ec192b7beb1
CesarGBkR <yexotortuesprin@gmail.com>
2022-07-26 10:49:32 -0500

    Variables y Constantes

---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero_1 = 8; # Sintaxis para asignar valor a una variable (VARIABLE, alias al que se le asigna un valor mutable)
+$numero_2 = 7;
+
+define("NUMERO_PI", 3.14); # Sintaxis para asignar un valor a una constante (CONSTANTE, alias al que se le asigna un valor inmutable)
+
+echo $numero_1 + $numero_2 ."\n";
+echo NUMERO_PI;
+echo "\n";
