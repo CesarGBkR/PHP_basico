cd77fd3178028884f19730dc6e3348a96198e9c1
CesarGBkR <yexotortuesprin@gmail.com>
2022-07-26 10:59:11 -0500

    Tipos

---
+++ tipos.php
@@ -0,0 +1,19 @@
+<?php
+
+$numero_1=1; # int
+var_dump($numero_1);
+
+$numero_2="2"; # string
+var_dump($numero_2);
+
+$numero_2=$numero_2 + $numero_1; # change to init 
+
+$numero_3 = 10; # int
+var_dump($numero_3);
+$numero_3 = $numero_3 + 0.5; # change to float
+var_dump($numero_3);
+
+var_dump($numero_1);
+var_dump($numero_2);
+
+echo "\n";
