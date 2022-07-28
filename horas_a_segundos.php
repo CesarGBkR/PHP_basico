db4ffdbc41523936f7dede8bffdf1aab01a67bd8
CesarGBkR <yexotortuesprin@gmail.com>
2022-07-28 15:39:25 -0500

    Adding horas_a_segundos excercise

--- horas_a_segundos.php
+++ horas_a_segundos.php
@@ -1,6 +1,8 @@
 <?php
-
 $horas = readline("Ingresa las horas a convertir: ");
-$segundos = (int)($horas * 3600);
-$minutos = ($horas * 60);
-echo "$horas horas $segundos segundos o también $minutos minutos";
+$minutos = readline("Ingresa los minutos a convertir: ");
+$segundos = readline("Ingresa los segundos a convertir: ");
+
+$totalSegundos = ($horas * 3600) + ($minutos * 60) + $segundos; 
+
+echo "La cantidad de segundos que hay en $horas horas es: $totalSegundos";
